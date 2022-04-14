1dadee4a1d236aa9a494628e2851aa78dbcfdcee
HymanChoi <HymanChoi@outlook.com>
2022-04-14 20:23:30 +0800

    1. 调整赛程模块样式；

--- src/views/schedule/index.vue
+++ src/views/schedule/index.vue
@@ -2,26 +2,28 @@
   <van-nav-bar title="Schedule" />
   <van-list class="list">
     <div
-      class="list-item"
+      class="list__item"
       :style="{ backgroundColor: item.bgc }"
       v-for="item of schedule"
       :key="item.cardTitle"
     >
-      <div class="list-item--content">
-        <div class="title">{{ item.cardTitle }}</div>
-        <div class="list-item--left">
+      <div class="list__item__content">
+        <div class="list__item__content-title">{{ item.cardTitle }}</div>
+        <div class="list__item__content-left">
           <van-image class="flag" height="20" :src="item.countryFlagSrc" />
-          <div class="date">
-            <span class="month">{{ item.month }}</span>
-            <span class="day"> {{ item.startDate }}-{{ item.endDate }} </span>
-          </div>
-          <div class="place">
+          <div class="list__item__place">
             {{ item.eventPlace }}
           </div>
-          <div class="desc">{{ item.eventTitle }}</div>
+          <div class="list__item__date">
+            <span class="list__item__month">{{ item.month }}</span>
+            <span class="list__item__day">
+              {{ item.startDate }}-{{ item.endDate }}
+            </span>
+          </div>
+          <div class="list__item__desc">{{ item.eventTitle }}</div>
         </div>
-        <div class="list-item--right">
-          <van-image height="80" :src="item.track" />
+        <div class="list__item__content-right">
+          <van-image v-if="item.track" height="80" :src="item.track" />
         </div>
       </div>
     </div>
@@ -58,69 +60,69 @@ export default defineComponent({
 });
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .list {
   height: calc(100vh - 96px);
   overflow-y: scroll;
-}
 
-.list-item--content {
-  position: relative;
-  display: flex;
-  align-items: center;
-  margin: 15px 10px;
-  padding: 15px 10px 10px;
-  border: 1px solid #e10600;
-  border-radius: 10px;
-  font-size: 14px;
-  text-align: left;
-}
+  .list__item__content {
+    position: relative;
+    display: flex;
+    align-items: center;
+    margin: 15px 10px;
+    padding: 15px 10px 10px;
+    border: 4px double #000;
+    border-radius: 10px;
+    font-size: 14px;
+    text-align: left;
 
-.title {
-  position: absolute;
-  top: -9px;
-  left: 40px;
-  padding: 0 10px;
-  background-color: #fff;
-  color: #e10600;
-  font-size: 16px;
-}
+    .list__item__content-title {
+      position: absolute;
+      top: -11px;
+      left: 30px;
+      padding: 0 10px;
+      background-color: #fff;
+      color: #e10600;
+      font-size: 16px;
+      font-weight: bold;
+    }
 
-.list-item--left {
-  position: relative;
-  flex: 1;
-}
+    .list__item__content-left {
+      position: relative;
+      flex: 1;
 
-.flag {
-  position: absolute;
-  top: 0;
-  right: 0;
-  border: 1px solid #eee;
-}
+      .list__item__place {
+        font-size: 18px;
+        margin-bottom: 8px;
+      }
 
-.date {
-  font-weight: bold;
-  font-size: 15px;
-  margin-bottom: 8px;
-}
+      .flag {
+        position: absolute;
+        top: 0;
+        right: 0;
+        border: 1px solid #eee;
+      }
 
-.month {
-  margin-right: 5px;
-}
+      .list__item__date {
+        font-weight: bold;
+        font-size: 15px;
+        margin-bottom: 8px;
+      }
 
-.place {
-  font-size: 18px;
-  margin-bottom: 8px;
-}
+      .list__item__month {
+        margin-right: 5px;
+      }
+    }
 
-.list-item--right {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  width: 150px;
-  height: 100px;
-  margin-left: 10px;
-  background: url(@/assets/img/plus.png) 8px repeat;
+    .list__item__content-right {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 150px;
+      height: 100px;
+      margin-left: 10px;
+      background: url(@/assets/img/plus.png) 8px repeat;
+    }
+  }
 }
 </style>
